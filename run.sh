5e2bdd6d419293247afe3faf17456d7545fc79d9
Michael Tong <michaelyuyangtong@gmail.com>
2021-08-04 17:16:10 +0800

    improved run script with command line arguments

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #! /bin/zsh
 
-read filename
-echo "File: $filename.sh"
-chmod +x $filename.sh
-./$filename.sh
+# pass in filename with command line arguments
+
+BGreen="\033[1;32m"
+NC="\033[0m"
+
+for script in "$@"
+  do
+    echo "\n${BGreen}File: $script${NC}"
+    chmod +x $script
+    if [ "${script:(-2)}" = "sh" ]; then
+      ./$script
+    fi
+done
