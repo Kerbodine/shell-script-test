c78169d35bf7b467963a7b77bcaa8f32645f8108
Michael Tong <michaelyuyangtong@gmail.com>
2021-08-04 11:13:32 +0800

    added fizzbuzz

---
+++ fizzbuzz.sh
@@ -0,0 +1,13 @@
+#! /bin/zsh
+
+for i in `seq 1 100`; do
+  if ! (($i % 15)); then
+    echo "fizzbuzz"
+  elif ! (($i % 3)); then
+    echo "fizz"
+  elif ! (($i % 5)); then
+    echo "buzz"
+  else
+    echo $i
+  fi
+done
